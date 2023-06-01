acf850dab9fe7fa9431f5b772e5e48380d0cdbe3
Jose Daniel Romero <danyrome12@gmail.com>
2023-06-01 01:54:52 -0500

    Se corrigieron algunas funciones globales y se declaro un nuevo objeto llamado planPrices, el cual contiene datos de costos de planes y adicionales

--- function.js
+++ function.js
@@ -10,29 +10,42 @@ let userData = {
     addOn3: false
 }
 
+const planPrices = {
+    arcade: 9,
+    advanced: 12,
+    pro: 15,
+    addOn1: 1,
+    addOn2and3: 2,
+}
+
+let tiempoPlan = "";
+let activePlanPrice;
+
 function monthlyState() {
     userData.yearly = false;
-    $('#arcade-subtitle').html('$9/mo');
-    $('#advanced-subtitle').html('$12/mo');
-    $('#pro-subtitle').html('$15/mo');
+    $('#arcade-subtitle').html(`$${planPrices.arcade}/mo`);
+    $('#advanced-subtitle').html(`$${planPrices.advanced}/mo`);
+    $('#pro-subtitle').html(`$${planPrices.pro}/mo`);
     $('.monthly-toggle').css("color","hsl(213, 96%, 18%)");
     $('.plan-bonus').css("display","none")
     $('.step-2-box').css("height","60%")
-    $('#addon-1-price').html('+$1/mo');
-    $('#addon-2-price, #addon-3-price').html('+$2/mo');
+    $('#addon-1-price').html(`+$${planPrices.addOn1}/mo`);
+    $('#addon-2-price, #addon-3-price').html(`+$${planPrices.addOn2and3}/mo`);
+    tiempoPlan = "Monthly";
 }
 
 function yearlyState() {
     userData.yearly = true;
-    $('#arcade-subtitle').html('$90/yr');
-    $('#advanced-subtitle').html('$120/yr');
-    $('#pro-subtitle').html('$150/yr');
+    $('#arcade-subtitle').html(`$${(planPrices.arcade)*10}/yr`);
+    $('#advanced-subtitle').html(`$${(planPrices.advanced)*10}/yr`);
+    $('#pro-subtitle').html(`$${(planPrices.pro)*10}/yr`);
     $('.yearly-toggle').css("color","hsl(213, 96%, 18%)");
     $('.monthly-toggle').css("color","");
     $('.plan-bonus').css("display","")
     $('.step-2-box').css("height","67.5%")
-    $('#addon-1-price').html('+$10/yr');
-    $('#addon-2-price, #addon-3-price').html('+$20/yr');
+    $('#addon-1-price').html(`+$${(planPrices.addOn1)*10}/yr`);
+    $('#addon-2-price, #addon-3-price').html(`+$${(planPrices.addOn2and3)*10}/yr`);
+    tiempoPlan = "Yearly";
 }
 
 $(function() {
@@ -69,27 +82,35 @@ $(function() {
         $(this).addClass("focus");
         $('#advanced-button').removeClass("focus");
         $('#pro-button').removeClass("focus");
-        userData.plan = "arcade";
+        userData.plan = "Arcade";
+        activePlanPrice = planPrices.arcade;
     })
 
     $('#advanced-button').click(function () {
         $(this).addClass("focus");
         $('#arcade-button').removeClass("focus");
         $('#pro-button').removeClass("focus");
-        userData.plan = "advanced";
+        userData.plan = "Advanced";
+        activePlanPrice = planPrices.advanced;
     })
 
     $('#pro-button').click(function () {
         $(this).addClass("focus");
         $('#advanced-button').removeClass("focus");
         $('#arcade-button').removeClass("focus");
-        userData.plan = "pro";
+        userData.plan = "Pro";
+        activePlanPrice = planPrices.pro;
     })
 
     $('#next-to-step-3').click(function() {
         $('#step-2').hide();
         $('#step-3').show();
         console.log(userData);
+        if (userData.yearly == true) {
+            summaryPlanPrice = (activePlanPrice)*10;
+        } else {
+            summaryPlanPrice = activePlanPrice;
+        }
     })
 
     $('#back-to-step-1').click(function() {
@@ -139,6 +160,37 @@ $(function() {
         $('#step-3').hide();
         $('#step-4').show();
         console.log(userData);
+
+        $('#summay-header-plan').html(`${userData.plan} (${tiempoPlan})`);
+        if (userData.yearly == true) {
+            $('#summay-header-price').html(`$${summaryPlanPrice}/yr`);
+        } else {
+            $('#summay-header-price').html(`$${summaryPlanPrice}/mo`);
+        }
+
+        if (userData.addOn1 == true) {
+            $('#addon-1-summary-title').html($('#addon-1-title').html());
+            $('#addon-1-summary-price').html($('#addon-1-price').html());
+        } else {
+            $('#addon-1-summary-title').html("");
+            $('#addon-1-summary-price').html("")
+        }
+
+        if (userData.addOn2 == true) {
+            $('#addon-2-summary-title').html($('#addon-2-title').html());
+            $('#addon-2-summary-price').html($('#addon-2-price').html());
+        } else {
+            $('#addon-2-summary-title').html("");
+            $('#addon-2-summary-price').html("")
+        }
+
+        if (userData.addOn3 == true) {
+            $('#addon-3-summary-title').html($('#addon-3-title').html());
+            $('#addon-3-summary-price').html($('#addon-3-price').html());
+        } else {
+            $('#addon-3-summary-title').html("");
+            $('#addon-3-summary-price').html("")
+        }
     })
 
     $('#back-to-step-2').click(function() {
